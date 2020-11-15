430d9f02047e1c7d7a546394f921418aa2dc3cd9
Kmaschta <kevin@tint.ai>
2020-11-15 11:53:27 +0100

    Configure prettier

--- src/index.js
+++ src/index.js
@@ -1,115 +1,109 @@
-const dateFormatter = new Intl.DateTimeFormat("fr", {
-  year: "numeric",
-  month: "numeric",
-  day: "numeric",
-  hour: "numeric",
-  minute: "numeric",
-  second: "numeric",
+const dateFormatter = new Intl.DateTimeFormat('fr', {
+    year: 'numeric',
+    month: 'numeric',
+    day: 'numeric',
+    hour: 'numeric',
+    minute: 'numeric',
+    second: 'numeric',
 });
 
 const getPosts = async () => {
-  const response = await fetch("/api/posts");
-  const posts = await response.json();
+    const response = await fetch('/api/posts');
+    const posts = await response.json();
 
-  return posts;
+    return posts;
 };
 
 const resetPage = () => {
-  document.querySelector("#app").innerHTML = "";
+    document.querySelector('#app').innerHTML = '';
 };
 
 const renderCreatePostPage = () => {
-  resetPage();
+    resetPage();
 
-  const form = document.createElement("form");
-  form.setAttribute("method", "POST");
-  form.setAttribute("action", "/api/posts");
+    const form = document.createElement('form');
+    form.setAttribute('method', 'POST');
+    form.setAttribute('action', '/api/posts');
 
-  form.innerHTML = `
-    <div class="form-group">
-      <label for="title-input">Titre</label>
-      <input type="text" class="form-control" id="title-input" name="title" />
-    </div>
-    <div class="form-group">
-      <label for="author-input">Auteur</label>
-      <input type="text" class="form-control" id="author-input" name="author" />
-    </div>
-    <div class="form-group">
-      <label for="body-input">Contenu</label>
-      <textarea class="form-control" id="body-input" name="body" rows="10"></textarea>
-    </div>
-    <button type="submit" class="btn btn-success">Publier</button>
-  `;
+    form.innerHTML = `
+        <div class="form-group">
+            <label for="title-input">Titre</label>
+            <input type="text" class="form-control" id="title-input" name="title" />
+        </div>
+        <div class="form-group">
+            <label for="author-input">Auteur</label>
+            <input type="text" class="form-control" id="author-input" name="author" />
+        </div>
+        <div class="form-group">
+            <label for="body-input">Contenu</label>
+            <textarea class="form-control" id="body-input" name="body" rows="10"></textarea>
+        </div>
+        <button type="submit" class="btn btn-success">Publier</button>
+    `;
 
-  document.querySelector("#app").appendChild(form);
+    document.querySelector('#app').appendChild(form);
 };
 
 const renderPostPage = (post) => {
-  resetPage();
+    resetPage();
 
-  const postContainer = document.createElement("div");
+    const postContainer = document.createElement('div');
 
-  postContainer.innerHTML = `
-    <div class="d-inline-flex justify-content-between mb-3">
-      <button id="back-button" class="btn btn-secondary">Retour</button>
-    </div>
-    <article class="card">
-      <div class="card-body">
-        <h3 class="card-title">${post.title}</h3>
-        <small>Publié le ${dateFormatter.format(
-          new Date(post.createdAt)
-        )}</small>
-        <p class="card-text mt-3">${post.body.split("\n").join("<br />")}</p>
-      </div>
-    </article>
-  `;
+    postContainer.innerHTML = `
+        <div class="d-inline-flex justify-content-between mb-3">
+            <button id="back-button" class="btn btn-secondary">Retour</button>
+        </div>
+        <article class="card">
+            <div class="card-body">
+                <h3 class="card-title">${post.title}</h3>
+                <small>Publié le ${dateFormatter.format(new Date(post.createdAt))}</small>
+                <p class="card-text mt-3">${post.body.split('\n').join('<br />')}</p>
+            </div>
+        </article>
+    `;
 
-  document.querySelector("#app").appendChild(postContainer);
-  document
-    .querySelector("#back-button")
-    .addEventListener("click", renderPostsPage);
+    document.querySelector('#app').appendChild(postContainer);
+    document.querySelector('#back-button').addEventListener('click', renderPostsPage);
 };
 
 const renderPostsPage = async () => {
-  resetPage();
+    resetPage();
 
-  const posts = await getPosts();
+    const posts = await getPosts();
 
-  const postsContainer = document.createElement("ul");
-  postsContainer.setAttribute("class", "list-group");
+    const postsContainer = document.createElement('ul');
+    postsContainer.setAttribute('class', 'list-group');
 
-  posts.forEach((post) => {
-    const postItem = document.createElement("li");
+    posts.forEach((post) => {
+        const postItem = document.createElement('li');
 
-    postItem.setAttribute(
-      "class",
-      "list-group-item d-inline-flex justify-content-between cursor-pointer"
-    );
+        postItem.setAttribute(
+            'class',
+            'list-group-item d-inline-flex justify-content-between cursor-pointer'
+        );
 
-    postItem.innerHTML = `
-        <div>
-            ${post.title}
-        </div>
-        <div class="text-right">
-            ${dateFormatter.format(new Date(post.createdAt))}
-        </div>
-    `;
+        postItem.innerHTML = `
+            <div>
+                ${post.title}
+            </div>
+            <div class="text-right">
+                ${dateFormatter.format(new Date(post.createdAt))}
+            </div>
+        `;
 
-    postItem.addEventListener("click", () => {
-      renderPostPage(post);
+        postItem.addEventListener('click', () => {
+            renderPostPage(post);
+        });
+
+        postsContainer.appendChild(postItem);
     });
 
-    postsContainer.appendChild(postItem);
-  });
-
-  document.querySelector("#app").appendChild(postsContainer);
+    document.querySelector('#app').appendChild(postsContainer);
 };
 
 // Wait for the document to be loaded before doing anything else
 window.onload = async () => {
-  await renderPostsPage();
+    await renderPostsPage();
 
-  document
-    .querySelector("#create-post-button")
-    .addEventListener("click", renderCreatePostPage);
+    document.querySelector('#create-post-button').addEventListener('click', renderCreatePostPage);
 };
